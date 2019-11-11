c79e9839ada2bdb02793b7f84c2ebf952f6d792c
1mm0rt41PC <guillaumenuel@hotmail.com>
2019-11-11 13:43:11 +0100

    Remove firewall disable

--- Harden/Windows/src/0-AutoUpdate.ps1
+++ Harden/Windows/src/0-AutoUpdate.ps1
@@ -2,6 +2,6 @@ $Trigger = New-ScheduledTaskTrigger -At 08:00am -Daily
 $Action  = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-exec AllSigned -nop -File C:\Windows\AutoHarden\AutoHarden.ps1"
 $Setting = New-ScheduledTaskSettingsSet -RestartOnIdle -StartWhenAvailable
 Register-ScheduledTask -TaskName "AutoHarden" -Trigger $Trigger -User "NT AUTHORITY\SYSTEM" -Action $Action -RunLevel Highest -Settings $Setting -Force
-netsh advfirewall set AllProfiles state off
+Get-NetFirewallRule -Name '*AutoHarden*Powershell*' | Disable-NetFirewallRule
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/1mm0rt41PC/HowTo/master/Harden/Windows/AutoHarden_RELEASE.ps1 -OutFile C:\Windows\AutoHarden\AutoHarden.ps1
-netsh advfirewall set AllProfiles state on
+Get-NetFirewallRule -Name '*AutoHarden*Powershell*' | Enable-NetFirewallRule
