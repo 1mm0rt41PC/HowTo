ec61e01f6e26f357763963d56c4312a05f51cb14
1mm0rt41PC <guillaumenuel@hotmail.com>
2019-11-11 14:15:51 +0100

    Allow excel and word only for local network

--- Harden/Windows/src/1-Hardening-Firewall.ps1
+++ Harden/Windows/src/1-Hardening-Firewall.ps1
@@ -1,52 +1,43 @@
 # Cleaning firewall rules
 netsh advfirewall set AllProfiles state on
 Set-NetFirewallProfile -DefaultInboundAction Block
-Get-NetFirewallRule | where { -not $_.Name.StartsWith("[AutoHarden-$AutoHarden_version]") -and -not $_.Name.StartsWith("[AutoHarden]") } | foreach { 
-	echo ('Cleaning old rules '+$_.Name)
-	$_ | Disable-NetFirewallRule
-	$_ | Remove-NetFirewallRule
-}
+echo 'Cleaning old rules ...'
+Get-NetFirewallRule | where { -not $_.Name.StartsWith("[AutoHarden-$AutoHarden_version]") -and -not $_.Name.StartsWith("[AutoHarden]") } | Remove-NetFirewallRule
 
-function blockExe( $name, $exe ){
+function blockExe( $name, $exe, [Parameter(Mandatory=$false)] $allowNonRoutableIP=$false ){
 	get-item $exe | foreach {
-		New-NetFirewallRule -direction Outbound -Action Block -Program $_.Fullname -Name ("[AutoHarden-$AutoHarden_version] "+$name+" : "+$_.Fullname) -DisplayName ("[AutoHarden-$AutoHarden_version] "+$name+" : "+$_.Fullname) -ErrorAction Ignore
+		$bin=$_.Fullname
+		if( $allowNonRoutableIP ){	
+			New-NetFirewallRule -direction Outbound -Action Block -Program $bin -RemoteAddress "Internet" -Name ("[AutoHarden-$AutoHarden_version][Except Intranet] "+$name+" : "+$bin) -DisplayName ("[AutoHarden-$AutoHarden_version][Except Intranet] "+$name+" : "+$bin) -ErrorAction Ignore
+		}else{
+			New-NetFirewallRule -direction Outbound -Action Block -Program $bin -Name ("[AutoHarden-$AutoHarden_version] "+$name+" : "+$bin) -DisplayName ("[AutoHarden-$AutoHarden_version] "+$name+" : "+$bin) -ErrorAction Ignore
+		}
 	}
 }
 
 if( (ask "Block communication for evil tools ?" "block-communication-for-powershell,eviltools.ask") -eq $true ){
-	blockExe "Powershell" "C:\Windows\WinSxS\*\powershell.exe"
-	blockExe "Powershell" "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
-	blockExe "WScript" "C:\Windows\system32\wscript.exe"
+	blockExe "Powershell" "C:\Windows\WinSxS\*\powershell.exe" $true
+	blockExe "Powershell" "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" $true
+	blockExe "WScript" "C:\Windows\system32\wscript.exe" $true
 	blockExe "BitsAdmin" "C:\Windows\system32\BitsAdmin.exe"
 	blockExe "Mshta" "C:\Windows\system32\mshta.exe"
 	blockExe "CertUtil" "C:\Windows\System32\certutil.exe"
 }else{
 	"Powershell", "WScript", "BitsAdmin", "Mshta", "CertUtil" | foreach {
-		$target=$_
-		Get-NetFirewallRule | where { $_.Name.StartsWith("[AutoHarden-$AutoHarden_version] $target :") } | foreach { 
-			echo ('Cleaning old rules '+$_.Name)
-			$_ | Disable-NetFirewallRule
-			$_ | Remove-NetFirewallRule
-		}
+		Get-NetFirewallRule -Name ("*AutoHarden*"+$_+"*") | Remove-NetFirewallRule
 	}
 }
 
 if( (ask "Block communication for Word and Excel ?" "block-communication-for-excel,word.ask") -eq $true ){
-	blockExe "Excel" "C:\Program Files*\Microsoft Office*\root\*\EXCEL.EXE"
-	blockExe "Excel" "C:\Program Files*\Microsoft Office*\*\root\*\EXCEL.EXE"
-	blockExe "Excel" "C:\Program Files*\Microsoft Office*\*\EXCEL.EXE"
-	blockExe "Word" "C:\Program Files*\Microsoft Office\root\*\winword.exe"
-	blockExe "Word" "C:\Program Files*\Microsoft Office\root\*\winword.exe"
-	blockExe "Word" "C:\Program Files*\Microsoft Office\root\*\winword.exe"
+	blockExe "Excel" "C:\Program Files*\Microsoft Office*\root\*\EXCEL.EXE" $true
+	blockExe "Excel" "C:\Program Files*\Microsoft Office*\*\root\*\EXCEL.EXE" $true
+	blockExe "Excel" "C:\Program Files*\Microsoft Office*\*\EXCEL.EXE" $true
+	blockExe "Word" "C:\Program Files*\Microsoft Office\root\*\winword.exe" $true
+	blockExe "Word" "C:\Program Files*\Microsoft Office\root\*\winword.exe" $true
+	blockExe "Word" "C:\Program Files*\Microsoft Office\root\*\winword.exe" $true
 }else{
-	"Excel", "Word" | foreach {
-		$target=$_
-		Get-NetFirewallRule | where { $_.Name.StartsWith("[AutoHarden-$AutoHarden_version] $target :") } | foreach { 
-			echo ('Cleaning old rules '+$_.Name)
-			$_ | Disable-NetFirewallRule
-			$_ | Remove-NetFirewallRule
-		}
-	}
+	Get-NetFirewallRule -Name '*AutoHarden*Excel*' | Remove-NetFirewallRule
+	Get-NetFirewallRule -Name '*AutoHarden*Word*' | Remove-NetFirewallRule
 }
 if( (Get-Item "C:\Program Files*\Nmap\nmap.exe") -ne $null ){
 	if( (ask "Allow NMAP to bypass the local firewall ?" "Allow-nmap.ask") -eq $true ){
@@ -54,14 +45,7 @@ if( (Get-Item "C:\Program Files*\Nmap\nmap.exe") -ne $null ){
 		New-NetFirewallRule -direction Outbound -Action Allow -Program $nmap -Name "[AutoHarden-$AutoHarden_version][OUT] NMAP bypass SNMP & co" -DisplayName "[AutoHarden-$AutoHarden_version][OUT] NMAP bypass SNMP & co" -ErrorAction Ignore
 		New-NetFirewallRule -direction Inbound -Action Allow -Program $nmap -Name "[AutoHarden-$AutoHarden_version][IN] NMAP bypass SNMP & co" -DisplayName "[AutoHarden-$AutoHarden_version][IN] NMAP bypass SNMP & co" -ErrorAction Ignore
 	}else{
-		"[OUT] NMAP", "[IN] NMAP" | foreach {
-			$target=$_
-			Get-NetFirewallRule | where { $_.Name.StartsWith("[AutoHarden-$AutoHarden_version] $target :") } | foreach { 
-				echo ('Cleaning old rules '+$_.Name)
-				$_ | Disable-NetFirewallRule
-				$_ | Remove-NetFirewallRule
-			}
-		}
+		Get-NetFirewallRule -Name '*AutoHarden*NMAP*' | Remove-NetFirewallRule
 	}
 }
 if( (Get-Item "C:\Program Files*\VMware\VMware Workstation\vmnat.exe") -ne $null ){
@@ -70,13 +54,6 @@ if( (Get-Item "C:\Program Files*\VMware\VMware Workstation\vmnat.exe") -ne $null
 		New-NetFirewallRule -direction Inbound -Action Allow -Program $vmware -Name "[AutoHarden-$AutoHarden_version][IN] VMWare bypass SNMP & co" -DisplayName "[AutoHarden-$AutoHarden_version][IN] VMWare bypass SNMP & co" -ErrorAction Ignore
 		New-NetFirewallRule -direction Outbound -Action Allow -Program $vmware -Name "[AutoHarden-$AutoHarden_version][OUT] VMWare bypass SNMP & co" -DisplayName "[AutoHarden-$AutoHarden_version][OUT] VMWare bypass SNMP & co" -ErrorAction Ignore
 	}else{
-		"[OUT] VMWare", "[IN] VMWare" | foreach {
-			$target=$_
-			Get-NetFirewallRule | where { $_.Name.StartsWith("[AutoHarden-$AutoHarden_version] $target :") } | foreach { 
-				echo ('Cleaning old rules '+$_.Name)
-				$_ | Disable-NetFirewallRule
-				$_ | Remove-NetFirewallRule
-			}
-		}	
+		Get-NetFirewallRule -Name '*AutoHarden*VMWare*' | Remove-NetFirewallRule
 	}
 }
